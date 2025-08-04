cb7791725476c45d51c30e42c376a73e338ebe20
Joao victor <j.17jvictor@gmail.com>
2025-08-04 20:57:49 -0300

    Desafio concluidogit add .!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia Nokia1 = new Nokia(numero: "982626387", modelo: "version1", imei: "1234", memoria: 64);
+Nokia1.Ligar();
+Nokia1.ReceberLigacao();
+Nokia1.InstalarAplicativo("Biblia");
+
+Console.WriteLine("============================================");
+
+Iphone Iphone1 = new Iphone(numero: "982626387", modelo: "XR", imei: "1234", memoria: 132);
+Iphone1.Ligar();
+Iphone1.ReceberLigacao();
+Iphone1.InstalarAplicativo("Netflix");
